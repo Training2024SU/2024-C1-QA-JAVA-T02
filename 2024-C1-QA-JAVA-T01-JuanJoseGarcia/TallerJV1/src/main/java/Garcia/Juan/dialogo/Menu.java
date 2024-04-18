95eddf02a5b3607e9ea39d1861b321212d94411b
DanielMorales <dm34mg@gmail.com>
2024-04-18 13:41:03 -0500

    Creacion del menu para la exportacion e importacion de archivos

--- 2024-C1-QA-JAVA-T01-JuanJoseGarcia/TallerJV1/src/main/java/Garcia/Juan/dialogo/Menu.java
+++ 2024-C1-QA-JAVA-T01-JuanJoseGarcia/TallerJV1/src/main/java/Garcia/Juan/dialogo/Menu.java
@@ -85,7 +85,16 @@ public class Menu {
     }
 
 
-
+    public static void menuImportExport() {
+        System.out.println(SELECCIONE);
+        System.out.println("1. Importar desde archivo CSV");
+        System.out.println("2. Exportar a archivo CSV");
+        System.out.println("3. Importar desde archivo XML");
+        System.out.println("4. Exportar a archivo XML");
+        System.out.println("5. Importar desde archivo JSON");
+        System.out.println("6. Exportar a archivo JSON");
+        System.out.println("7. " + SALIR);
+    }
 
 
 
