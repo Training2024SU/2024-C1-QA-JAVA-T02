0b767c973aa11b7bae29c513c9617764c619e28a
DanielMorales <dm34mg@gmail.com>
2024-04-17 21:00:36 -0500

    Se realizo la conexion a la base de datos

--- 2024-C1-QA-JAVA-T01-JuanJoseGarcia/TallerJV1/src/main/java/Garcia/Juan/database/ConexionBD.java
+++ 2024-C1-QA-JAVA-T01-JuanJoseGarcia/TallerJV1/src/main/java/Garcia/Juan/database/ConexionBD.java
@@ -5,10 +5,22 @@ import java.sql.SQLException;
 
 public class ConexionBD {
 
-    private static final String SERVER = System.getenv("GRADLE_SERVER");
-    private static final String DATA_BASE_NAME = System.getenv("GRADLE_DB");
-    private static final String USER = System.getenv("GRADLE_USER");
-    private static final String PASSWORD = System.getenv("GRADLE_PASS");
+    private static final String SERVER = "localhost";
+    private static final String DATA_BASE_NAME = "bibliotecapingu";
+    private static final String USER = "root";
+
+    // Escapa la contraseña usando URLEncoder.encode en la declaración de la constante
+    private static final String PASSWORD;
+
+    static {
+        try {
+            PASSWORD = java.net.URLEncoder.encode("#32zvv48dH", "UTF-8");
+        } catch (java.io.UnsupportedEncodingException e) {
+            // Maneja la excepción si ocurre un error al codificar la contraseña
+            throw new RuntimeException("Error al codificar la contraseña en UTF-8", e);
+        }
+    }
+
 
     private static MySqlOperation mySqlOperation;
 
